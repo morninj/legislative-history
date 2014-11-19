14e70e37bfd63635d9c53a83bffe3d2d57a7f42c
Joseph Mornin <joseph@mornin.org>
2014-11-18 21:04:25 -0800

    Enhance Legislation model

--- legislativehistory/models.py
+++ legislativehistory/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 class Legislation(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.CharField(max_length=255) # not SlugField because GAE doesn't support it
     state = models.CharField(max_length=100)
     bill_number = models.CharField(max_length=100)
-    name = models.CharField(max_length=100)
     session_year = models.CharField(max_length=20)
     def __unicode__(self):
         return self.bill_number + ' (' + self.state + ')'
-
+    def save(self, *args, **kwargs):
+        self.slug = str(slugify(self.bill_number))
+        super(Legislation, self).save(*args, **kwargs)
