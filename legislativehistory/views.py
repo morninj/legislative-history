ff24d7852bd96cd3f78182271480db0e46da1a72
Joseph Mornin <joseph@mornin.org>
2014-11-18 21:28:05 -0800

    Enhance legislation view

--- legislativehistory/views.py
+++ legislativehistory/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from legislativehistory.models import *
 
 def index(request):
     return render(request, 'index.html')
 
-def bill_detail(request, state, session):
-    return render(request, 'bill_detail.html', {'state': state, 'session': session})
+def bill_detail(request, state_slug, session, bill_number_slug):
+    # TODO change all refs to "session_year" to "session"
+    l = get_object_or_404(Legislation, state_slug=state_slug, session_year=session, bill_number_slug=bill_number_slug)
+    return render(request, 'bill_detail.html', {'legislation': l})
 
 def legislation(request):
     legislation = Legislation.objects.all()
--- scaffold/urls.py
+++ scaffold/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     url(r'^_ah/', include('djangae.urls')),
     url(r'^$', 'legislativehistory.views.index'),
     url(r'^legislation/$', 'legislativehistory.views.legislation'),
-    url(r'^(?P<state>[a-zA-Z]+)/(?P<session>[0-9|-]+)/$', 'legislativehistory.views.bill_detail'),
+    url(r'^(?P<state_slug>[a-zA-Z]+)/(?P<session>[0-9|-]+)/(?P<bill_number_slug>[a-z|A-Z|0-9|-]+)/$', 'legislativehistory.views.bill_detail'),
 
     # Note that by default this is also locked down with login:admin in app.yaml
     url(r'^admin/', include(admin.site.urls)),
