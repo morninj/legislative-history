408af3dcaf5d069e8b7320662e2074472c71e21d
Joseph Mornin <joseph@mornin.org>
2014-11-18 19:20:36 -0800

    Add basic bill model

--- legislativehistory/models.py
+++ legislativehistory/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Legislation(models.Model):
+    state = models.CharField(max_length=100)
+    bill_number = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    session_year = models.CharField(max_length=20)
+    def __unicode__(self):
+        return self.bill_number + ' (' + self.state + ')'
+
