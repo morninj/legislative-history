b63f87bb2f513a72504e69a6c1237d4099162d34
Joseph Mornin <joseph@mornin.org>
2014-11-18 20:22:54 -0800

    Add boxes on home page

--- legislativehistory/views.py
+++ legislativehistory/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from legislativehistory.models import *
 
 def index(request):
-    return render(request, 'index.html')
+    legislation = Legislation.objects.get()
+    return render(request, 'index.html', {'legislation': legislation})
+
